9c2588c1b779df8e1ba54c6bd9d23e9b3a6cdd3d
Borja321 <borja370@gmail.com>
2016-12-16 08:16:26 +0100

    Update Frabricantes.java

--- src/javaapplication2/Frabricantes.java
+++ src/javaapplication2/Frabricantes.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package javaapplication2;
-
+/* Esto lo he hecho desde GitHub */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
